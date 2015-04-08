4b54b7a5da19ea04095b5e81ccf6c79fb6b65ebc
Carlson Go <carlson.go@eastvantage.com>
2015-04-08 13:45:53 +0800

    initial commit

---
+++ BusyBongo.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace BusyBongo.Web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include("~/Scripts/bootstrap.js"));            
+
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+        }
+    }
+}
---
+++ BusyBongo.Web/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using BusyBongo.Web.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace BusyBongo.Web
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            // Drop the WebForms view engine
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
