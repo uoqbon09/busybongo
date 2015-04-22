3e653699c59919be3f6f6c0bd0ecf02deb8cb33c
Carlson Go <carlson.go@eastvantage.com>
2015-04-22 16:09:41 +0800

    -used cdns

--- BusyBongo.Web/App_Start/BundleConfig.cs
+++ BusyBongo.Web/App_Start/BundleConfig.cs
@@ -7,9 +7,9 @@ namespace BusyBongo.Web.App_Start
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/Scripts/libs")
-                .Include("~/Scripts/jquery-{version}.js")                
-                .Include("~/Scripts/knockout-{version}.js")
-                .Include("~/Scripts/bootstrap.js")
+                //.Include("~/Scripts/jquery-{version}.js")                
+                //.Include("~/Scripts/knockout-{version}.js")
+                //.Include("~/Scripts/bootstrap.js")
                 .Include("~/Scripts/Stashy.js")
                 );            
 
