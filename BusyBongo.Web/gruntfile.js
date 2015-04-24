c8b2f5010a0da3a7fcd5c5de1af25f007d8ab25c
Carlson Go <carlson.go@eastvantage.com>
2015-04-24 19:27:05 +0800

    -added grunt dependencies, uglified durandal main.js

---
+++ BusyBongo.Web/gruntfile.js
@@ -0,0 +1,41 @@
+﻿module.exports = function (grunt) {
+    'use strict';
+    var mixIn = require('mout/object/mixIn');
+    var requireConfig = {
+        baseUrl: 'app/',
+        paths: {
+            'jquery': '../Scripts/jquery-2.1.3',
+            'knockout': '../Scripts/knockout-3.3.0',
+            'text': '../Scripts/text',
+            'durandal': '../Scripts/durandal',
+            'plugins': '../Scripts/durandal/plugins',
+            'transitions': '../Scripts/durandal/transitions'
+        }
+    };
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        durandal: {
+            main: {
+                src: ['app/**/*.*', 'Scripts/durandal/**/*.js'],
+                options: {
+                    name: '../Scripts/almond-custom',
+                    baseUrl: requireConfig.baseUrl,
+                    mainPath: 'app/main',
+                    paths: mixIn({}, requireConfig.paths, { 'almond': '../Scripts/almond-custom.js' }),
+                    exclude: [],
+                    optimize: 'none',
+                    out: 'build/app/main.js'
+                }
+            }
+        },
+        uglify: {
+            build: {
+                src: 'build/app/main.js',
+                dest: 'build/app/main-built.js'
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-durandal');
+    grunt.loadNpmTasks('grunt-contrib-uglify');    
+    grunt.registerTask('build', ['durandal:main','uglify']);
+};
