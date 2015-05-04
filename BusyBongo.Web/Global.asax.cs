a1b14b006304f7b96fe3b76b643ff27f4267be8f
Carlson Go <carlson.go@eastvantage.com>
2015-05-04 14:43:07 +0800

    -added tracing

--- BusyBongo.Web/Global.asax.cs
+++ BusyBongo.Web/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using BusyBongo.Web.App_Start;
 using System.Configuration;
+using System.Diagnostics;
 using System.Reflection;
 using System.Web.Configuration;
 using System.Web.Mvc;
@@ -28,24 +29,44 @@ namespace BusyBongo.Web
             if (Context.IsDebuggingEnabled)
             {
                 return;
+            }            
+
+            try
+            {
+                Trace.TraceInformation("Start ResetMachineKey()");
+
+                var mksType = typeof(MachineKeySection);
+                var mksSection = ConfigurationManager.GetSection("system.web/machineKey") as MachineKeySection;
+                var resetMethod = mksType.GetMethod("Reset", BindingFlags.NonPublic | BindingFlags.Instance);
+
+                var newConfig = new MachineKeySection();
+                newConfig.ApplicationName = mksSection.ApplicationName;
+                newConfig.CompatibilityMode = mksSection.CompatibilityMode;
+                newConfig.DataProtectorType = mksSection.DataProtectorType;
+                newConfig.Validation = mksSection.Validation;
+
+                Trace.TraceInformation("Decryption: " + mksSection.Decryption);
+                Trace.TraceInformation("DecryptionKey: " + mksSection.DecryptionKey);
+                Trace.TraceInformation("Validation: " + mksSection.Validation);
+                Trace.TraceInformation("ValidationKey: " + mksSection.ValidationKey);
+
+                newConfig.ValidationKey = ConfigurationManager.AppSettings["MKEY_VALIDATIONKEY"];
+                newConfig.DecryptionKey = ConfigurationManager.AppSettings["MKEY_DECRYPTIONKEY"];
+                newConfig.Decryption = ConfigurationManager.AppSettings["MKEY_DECRYPTION"]; // default: AES
+                newConfig.ValidationAlgorithm = ConfigurationManager.AppSettings["MKEY_VALIDATION"]; // default: SHA1
+
+                resetMethod.Invoke(mksSection, new object[] { newConfig });
+
+                Trace.TraceInformation("Decryption: " + mksSection.Decryption);
+                Trace.TraceInformation("DecryptionKey: " + mksSection.DecryptionKey);
+                Trace.TraceInformation("Validation: " + mksSection.Validation);
+                Trace.TraceInformation("ValidationKey: " + mksSection.ValidationAlgorithm);
             }
-
-            var mksType = typeof(MachineKeySection);
-            var mksSection = ConfigurationManager.GetSection("system.web/machineKey") as MachineKeySection;
-            var resetMethod = mksType.GetMethod("Reset", BindingFlags.NonPublic | BindingFlags.Instance);
-
-            var newConfig = new MachineKeySection();
-            newConfig.ApplicationName = mksSection.ApplicationName;
-            newConfig.CompatibilityMode = mksSection.CompatibilityMode;
-            newConfig.DataProtectorType = mksSection.DataProtectorType;
-            newConfig.Validation = mksSection.Validation;
-
-            newConfig.ValidationKey = ConfigurationManager.AppSettings["MKEY_VALIDATIONKEY"];
-            newConfig.DecryptionKey = ConfigurationManager.AppSettings["MKEY_DECRYPTIONKEY"];
-            newConfig.Decryption = ConfigurationManager.AppSettings["MKEY_DECRYPTION"]; // default: AES
-            newConfig.ValidationAlgorithm = ConfigurationManager.AppSettings["MKEY_VALIDATION"]; // default: SHA1
-
-            resetMethod.Invoke(mksSection, new object[] { newConfig });
+            finally
+            {
+                Trace.TraceInformation("End ResetMachineKey()");
+            }
+            
         }
     }
 }
