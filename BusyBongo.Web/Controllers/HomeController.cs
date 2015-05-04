cf3ea71d924eb80a81dd3a6353b404006fbae0f9
Carlson Go <carlson.go@eastvantage.com>
2015-05-04 15:05:18 +0800

    -temp trace for machinekey

--- BusyBongo.Web/Controllers/HomeController.cs
+++ BusyBongo.Web/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Diagnostics;
+using System.Web.Configuration;
+using System.Web.Mvc;
 
 namespace BusyBongo.Web.Controllers
 {
@@ -7,6 +10,12 @@ namespace BusyBongo.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            var mksSection = ConfigurationManager.GetSection("system.web/machineKey") as MachineKeySection;
+            Trace.TraceInformation("Decryption: " + mksSection.Decryption);
+            Trace.TraceInformation("DecryptionKey: " + mksSection.DecryptionKey);
+            Trace.TraceInformation("Validation: " + mksSection.ValidationAlgorithm);
+            Trace.TraceInformation("ValidationKey: " + mksSection.ValidationKey);
+
             return View();
         }
     }
